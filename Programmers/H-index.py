a0adec19554e956901d9c036d6c4b39414152f1c
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-11-09 19:43:59 +0900

    H-index_OK

--- Programmers/H-index.py
+++ Programmers/H-index.py
@@ -1,31 +1,8 @@
 def solution(citations):
-    answer = 0
-
-    big = 0
-    small = 0
-
     citations.sort()
+    article_count = len(citations)
 
-    n = len(citations)
-
-    for i in range(n):
-        
-        big = 0
-        small = 0
-
-        for j in citations:
-
-            if(j < i):
-                small+=1
-            elif(j > i):
-                big+=1
-            elif (j == i):
-                big+=1
-                small+=1
-            
-            if(i == big and i == small):
-                answer = i
-
-    return answer
-
-print(solution([0, 1, 2, 5, 3]))
+    for i in range(article_count):
+        if citations[i] >= article_count-i:
+            return article_count-i
+    return 0

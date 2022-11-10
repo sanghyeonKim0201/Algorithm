433b9e0722828e06abd2783e5b4f0760c59f85cc
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-11-10 21:06:19 +0900

    최소직사각형_OK

--- Programmers/최소직사각형.py
+++ Programmers/최소직사각형.py
@@ -4,16 +4,15 @@ def solution(sizes):
     max_arr = []
     min_arr = []
 
+
     for i in range(len(sizes)):
 
         if sizes[i][0] > sizes[i][1]:
             max_arr.append(sizes [i][0])
-        if sizes[i][0] < sizes[i][1]:
-            min_arr.append(sizes[i][0])
-        if sizes[i][1] > sizes[i][0]:
-            max_arr.append(sizes[i][1])
-        if sizes[i][1] < sizes[i][0]:
             min_arr.append(sizes[i][1])
+        else:
+            max_arr.append(sizes[i][1])
+            min_arr.append(sizes[i][0])
 
     maxNum = max(max_arr)
     minNum = max(min_arr)

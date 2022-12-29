259726b5f22746c96de2f0e0656e29a78268cd03
sanghyeonKim0201 <gmhty4345@gmail.com>
2022-12-29 10:06:04 +0900

    Baek_1809_OK

---
+++ Baekjoon/src/bronze/Baek_1809_OK.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+
+a = [list(map(int, input().split())) for i in range(n)]
+b = [list(map(int, input().split())) for i in range(n)]
+
+for i in range(n):
+    for j in range(m):
+        a[i][j] += b[i][j]
+        print(a[i][j])
+    

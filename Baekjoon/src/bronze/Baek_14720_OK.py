f4a2679403e3eba4f8407b8070b40a9641949dfe
sanghyeonKim0201 <gmhty4345@gmail.com>
2023-01-03 19:50:38 +0900

    Baek_14720_OK

---
+++ Baekjoon/src/bronze/Baek_14720_OK.py
@@ -0,0 +1,13 @@
+n = int(input())
+shops = list(map(int, input().split()))
+milk = [0, 1, 2]
+
+num = 0
+
+for i in range(n):
+    if(shops[i] == milk[num%3]):
+        num+=1
+    
+print(num)
+
+    

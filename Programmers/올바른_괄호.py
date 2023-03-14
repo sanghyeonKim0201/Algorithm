85f1b790f1d0733bb5df443abc308d8eb3b14f81
sanghyeonKim0201 <gmhty4345@gmail.com>
2023-03-14 17:22:30 +0900

    올바른 괄호

---
+++ Programmers/올바른_괄호.py
@@ -0,0 +1,9 @@
+def solution(s):
+    x = 0
+    for w in s:
+        if x < 0:
+            break
+        x = x+1 if w=="(" else x-1 if w==")" else x
+    return x==0
+
+print(solution("()()()()())))(()()))()((())()((((())()(()((()))))(()))((((("))
